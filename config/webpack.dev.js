d67868a9d27530e999b6607389a8024cb49cda10
Andrii Kvach <0968512988z@gmail.com>
2021-04-19 20:43:00 +0300

    fix

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,8 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
 const merge = require("webpack-merge");
-const { basename } = require('path')
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const webpackCommon = require("./webpack.common");
 
 module.exports = merge.smart(webpackCommon, {
@@ -10,25 +8,19 @@ module.exports = merge.smart(webpackCommon, {
   output: {
     publicPath: "/", // deploy on server with /app/ folder name
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "../docs")
+    path: path.resolve(__dirname, "../build")
   },
   devServer: {
     hot: true,
     open: true,
-    historyApiFallback: true,
     overlay: true,
-    contentBase: path.join(__dirname, "docs"),
+    contentBase: path.join(__dirname, "build"),
+    
     host: "localhost",
     port: 3000,
-    publicPath: "/"
   },
   devtool: "cheap-eval-source-map",
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new FaviconsWebpackPlugin({
-      logo: path.resolve(__dirname, "../src/favicon.ico"),
-      inject: htmlPlugin => 
-        basename(htmlPlugin.options.filename) === 'index.html'      
-    })
   ]
 });
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,48 +1,26 @@
-const path = require("path");
-const merge = require("webpack-merge");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const CleanWbepackPlugin = require("clean-webpack-plugin");
-
-const webpackCommon = require("./webpack.common");
-
-const pathsToClean = ["docs"];
-const cleanOptions = {
-  root: path.resolve(__dirname, "../")
-};
+const path = require('path');
+const merge = require('webpack-merge');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpackCommon = require('./webpack.common');
 
 module.exports = merge.smart(webpackCommon, {
-  output: {
-    filename: "main.[chunkhash].js",
-    path: path.resolve(__dirname, "../docs"),
-    publicPath: "/"
-  },
-  mode: "production",
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          mangle: {
-            keep_fnames: true
-          }
-        }
-      })
-    ],
-    runtimeChunk: true,
-    splitChunks: {
-      cacheGroups: {
-        default: false,
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendor_app",
-          chunks: "all",
-          minChunks: 2
-        }
-      }
-    }
-  },
-  plugins: [
-    new CleanWbepackPlugin(pathsToClean, cleanOptions),
-    new OptimizeCssAssetsPlugin()
-  ]
-});
+	output: {
+		filename: '[name].[chunkhash].js',
+		path: path.resolve(__dirname, '../build'),
+		publicPath: ''
+	},
+	mode: 'production',
+	optimization: {
+		minimizer: [
+			new TerserPlugin({
+				sourceMap: true,
+			}),
+		],
+	},
+	plugins: [
+		new OptimizeCssAssetsPlugin(),
+		new CleanWebpackPlugin(),
+	]
+})
